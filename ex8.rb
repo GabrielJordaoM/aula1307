37e4bd8a1f7a14aaf621a467b73369ebc9ee88ba
Gabriel Jordão <54850185+GabrielJordaoM@users.noreply.github.com>
2023-07-14 19:13:53 -0300

    Update ex8.rb
    metodo alternativo

--- ex8.rb
+++ ex8.rb
@@ -8,8 +8,13 @@ def nomes(lista_nomes)
     nomes_compridos
   end
   
-  # Exemplo de uso
   pessoas = ["João", "Maria", "Pedro", "Ana", "Lucas", "Carolina", "Gabriel", "Gustavo", "Renan", "Lucas"]
   nomes_compridos = nomes(pessoas)
   puts nomes_compridos.inspect
-  
+
+def select(nomes)
+    nomes.select [ |nome| nome.length > 5 ]
+end
+
+lista = ["João", "Maria", "Pedro", "Ana", "Lucas", "Carolina", "Gabriel", "Gustavo", "Renan", "Lucas"]
+puts select(lista).inspect
