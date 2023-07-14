f49de670b37d2408876f74a40ba8dac1f731cc1e
Gabriel Jordão <54850185+GabrielJordaoM@users.noreply.github.com>
2023-07-14 19:08:40 -0300

    Update ex3.rb
    metodo alternativo de fazer

--- ex3.rb
+++ ex3.rb
@@ -1,2 +1,9 @@
-arr = [5, 6, 7, 8]
-puts arr.inject{ |sum, el| sum + el }.to_f / arr.size
+# arr = [5, 6, 7, 8]
+# puts arr.inject{ |sum, el| sum + el }.to_f / arr.size
+def media(lista)
+  soma = lista.reduce(:+)
+  media = soma.to_f / lista.length
+  return media
+end
+numeros = [10, 5, 8, 2, 15]
+puts calcular_media(numeros)
